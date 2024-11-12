a7bdc4100d98fbca542f19531e1b348e81f6b7dc
Andre Luiz <andre.ferreira@univision.net.br>
2024-11-12 20:39:56 -0300

    fix/ajustes finais

--- auth.js
+++ auth.js
@@ -3,10 +3,9 @@ document.addEventListener("DOMContentLoaded", function () {
   const loginButton = document.getElementById("btn-login");
   const logoutButton = document.getElementById("btn-logout");
   const usernameElement = document.getElementById("username");
-  const user = JSON.parse(sessionStorage.getItem("user"));
-
   // Função para verificar se existe um login no sessionStorage
   function checkLogin() {
+    const user = JSON.parse(sessionStorage.getItem("user"));
     if (user) {
       profileDiv.style.display = "flex";
       loginButton.style.display = "none";
@@ -14,25 +13,26 @@ document.addEventListener("DOMContentLoaded", function () {
       profileDiv.style.display = "none";
       loginButton.style.display = "flex";
     }
+    return user;
   }
 
   // Função para exibir o nome do usuário
-  function displayUsername() {
+  function displayUsername(user) {
     if (usernameElement && user) {
-      usernameElement.innerText = user.nome;
+      usernameElement.innerText = user.nome || user.name;
     }
   }
 
   // Função para sair
   function logout() {
     sessionStorage.removeItem("user");
-    location.reload();
+    location.replace("/");
   }
 
   // Adicionar evento de clique ao botão de logout
   logoutButton.addEventListener("click", logout);
 
   // Verificar login ao carregar a página
-  checkLogin();
-  displayUsername();
+  const user = checkLogin();
+  displayUsername(user);
 });
